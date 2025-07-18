181311708ad26d574a21a0c7c2e911007958fe22
Wybie <lyrncofyn@gmail.com>
2025-07-18 17:18:36 +0800

    refactor(auth-api): Move all auth related route to a new file inside api folder called auth

---
+++ routes/api/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
