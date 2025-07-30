e00c6499a43f5674086c872514271f6b08b0b5a4
Wybie <lyrncofyn@gmail.com>
2025-07-30 20:41:17 +0800

    feat(api): Add badge related api

--- routes/api/gamification.php
+++ routes/api/gamification.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\BadgeController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'ability:admin:*'])->group(function () {
     Route::apiResource('gamification/levels', LevelController::class);
+    Route::apiResource('gamification/badges', BadgeController::class);
+    Route::get('gamification/badge-categories', [BadgeController::class, 'getBadgeCategories']);
 });
