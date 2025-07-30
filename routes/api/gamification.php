7dc5a4ed6c7443d39e2811f10361f241df364ca2
Wybie <lyrncofyn@gmail.com>
2025-07-31 02:15:35 +0800

    feat(achievement-api): Add all achievement resource to the api.

--- routes/api/gamification.php
+++ routes/api/gamification.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AchievementController;
 use App\Http\Controllers\BadgeController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,6 @@ Route::middleware(['auth:sanctum', 'ability:admin:*'])->group(function () {
     Route::apiResource('gamification/levels', LevelController::class);
     Route::apiResource('gamification/badges', BadgeController::class);
     Route::get('gamification/badge-categories', [BadgeController::class, 'getBadgeCategories']);
+    Route::apiResource('gamification/achievements', AchievementController::class);
+    Route::get('gamification/achievement-types', [AchievementController::class, 'getAchievementTypes']);
 });
