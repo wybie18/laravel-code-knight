a69cc9d1fb690785e70ec92a40e8ba3f78ab7ba9
Wybie <lyrncofyn@gmail.com>
2025-08-10 22:26:22 +0800

    refactor(coding-challenge-resource): Update the resource to much cleaner and move the extra checking to programming languages resource

--- app/Http/Resources/CodingChallengeResource.php
+++ app/Http/Resources/CodingChallengeResource.php
@@ -1,8 +1,6 @@
 <?php
-
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CodingChallengeResource extends JsonResource
@@ -14,37 +12,16 @@ class CodingChallengeResource extends JsonResource
      */
     public function toArray($request)
     {
-        $data = [
-            'id'                => $this->id,
-            'problem_statement' => $this->problem_statement,
-            'programming_languages' => ProgrammingLanguageResource::collection($this->whenLoaded('programmingLanguages')),
+        return [
+            'id'                    => $this->id,
+            'problem_statement'     => $this->problem_statement,
+            'test_cases'            => $this->when(
+                $request->user() && $request->user()->tokenCan('admin:*'),
+                json_decode($this->test_cases)
+            ),
+            'programming_languages' => ProgrammingLanguageResource::collection(
+                $this->whenLoaded('programmingLanguages')
+            ),
         ];
-
-        if ($request->user() && $request->user()->tokenCan('admin:*')) {
-            $data['test_cases'] = json_decode($this->test_cases);
-            $data['programming_languages'] = $this->whenLoaded('programmingLanguages', function () {
-                return $this->programmingLanguages->map(function ($lang) {
-                    return [
-                        'id'           => $lang->id,
-                        'name'         => $lang->name,
-                        'starter_code' => $lang->pivot->starter_code,
-                        'solution_code' => $lang->pivot->solution_code,
-                    ];
-                });
-            });
-        } else {
-            $data['test_cases'] = null;
-            $data['programming_languages'] = $this->whenLoaded('programmingLanguages', function () {
-                return $this->programmingLanguages->map(function ($lang) {
-                    return [
-                        'id'           => $lang->id,
-                        'name'         => $lang->name,
-                        'starter_code' => $lang->pivot->starter_code,
-                    ];
-                });
-            });
-        }
-
-        return $data;
     }
 }
