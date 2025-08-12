0f8934ff3cf404bd479887f092d8b7dc3b275385
Wybie <lyrncofyn@gmail.com>
2025-08-13 02:41:56 +0800

    feat(auth-api): Add all the profile editing api url in the auth api.

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/profile/update', [ProfileController::class, 'update']);
+    Route::put('/password', [PasswordController::class, 'update']);
+    Route::delete('/profile/delete', [ProfileController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
