a4d68db75419363859184c91cad2e901b4e6ac9a
Wybie <lyrncofyn@gmail.com>
2025-08-13 02:56:31 +0800

    feat(course-module): Added new model to the project and this will help group the lessons.

---
+++ database/migrations/2025_07_05_155244_create_course_modules_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_modules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->integer('order');
+            $table->timestamps();
+
+            $table->unique(['course_id', 'order']);
+            $table->unique(['course_id', 'slug']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_modules');
+    }
+};
