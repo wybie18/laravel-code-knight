a55ad37fb41add751279ed9691f84126dae332c9
Wybie <lyrncofyn@gmail.com>
2025-08-17 21:54:20 +0800

    feat(lesson-resource): Added new api resource for lesson.

---
+++ app/Http/Resources/LessonResource.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LessonResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'                 => $this->id,
+            'title'              => $this->title,
+            'slug'               => $this->slug,
+            'content'            => $this->content,
+            'video_url'          => $this->video_url,
+            'exp_reward'         => $this->exp_reward,
+            'estimated_duration' => $this->estimated_duration,
+            'order'              => $this->order,
+            'is_published'       => $this->is_published,
+            'created_at'         => $this->created_at,
+            'updated_at'         => $this->updated_at,
+
+            // Relationships
+            'module'             => new CourseModuleResource($this->whenLoaded('module')),
+            'course'             => new CourseResource($this->whenLoaded('course')),
+            'activities'         => $this->activities,
+            'prerequisites'      => LessonResource::collection($this->whenLoaded('prerequisites')),
+            'progress'           => new UserLessonProgressResource($this->whenLoaded('userProgress')),
+
+            // Computed fields
+            'activities_count'   => $this->when($this->activities_count !== null, $this->activities_count),
+            'is_accessible'      => $this->when(isset($this->is_accessible), $this->is_accessible),
+            'is_completed'       => $this->when(isset($this->is_completed), $this->is_completed),
+        ];
+    }
+}
