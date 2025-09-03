816f7b4856f1d002a6cfcc136adb520e35e57a08
Wybie <lyrncofyn@gmail.com>
2025-09-03 14:44:16 +0800

    feat(challenges-api): New routes for view all, show, and submitting challenge for ctf and coding.

--- routes/api/challenges.php
+++ routes/api/challenges.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Challenge\ChallengeCodeExecutionController;
 use App\Http\Controllers\Challenge\CodingChallengeController;
 use App\Http\Controllers\Challenge\CtfChallengeController;
+use App\Http\Controllers\Challenge\CtfSubmissionController;
 use App\Http\Controllers\Challenge\TypingChallengeController;
 use App\Http\Controllers\CtfCategoryController;
 use App\Http\Controllers\DifficultyController;
@@ -13,7 +15,17 @@ Route::get('ctf-categories/all', [CtfCategoryController::class, 'getAllCtfCatego
 Route::get('programming-languages/all', [ProgrammingLanguageController::class, 'getProgrammingLanguages']);
 
 Route::middleware(['auth:sanctum', 'ability:admin:*'])->group(function () {
-    Route::apiResource('challenges/ctf', CtfChallengeController::class);
-    Route::apiResource('challenges/coding', CodingChallengeController::class);
+    Route::apiResource('challenges/ctf', CtfChallengeController::class)->except(['index', 'show']);
+    Route::apiResource('challenges/coding', CodingChallengeController::class)->except(['index', 'show']);
     Route::apiResource('challenges/typing', TypingChallengeController::class);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('challenges/ctf', [CtfChallengeController::class, 'index']);
+    Route::get('challenges/ctf/{ctf}', [CtfChallengeController::class, 'show']);
+    Route::post('challenges/ctf/{ctf}/submit', [CtfSubmissionController::class, 'store']);
+
+    Route::get('challenges/coding', [CodingChallengeController::class, 'index']);
+    Route::get('challenges/coding/{coding}', [CodingChallengeController::class, 'show']);
+    Route::post('challenges/coding/{coding}/execute-code', [ChallengeCodeExecutionController::class, 'executeCode']);
 });
