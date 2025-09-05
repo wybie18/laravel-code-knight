314ff3433ca23c3cbffc94cf2397528b09af5223
Wybie <lyrncofyn@gmail.com>
2025-09-06 04:36:50 +0800

    feat(challenges-api): Separate the routes index and show in the apiResource and add these to the separated middleware group.

--- routes/api/challenges.php
+++ routes/api/challenges.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Challenge\CodingChallengeController;
 use App\Http\Controllers\Challenge\CtfChallengeController;
 use App\Http\Controllers\Challenge\CtfSubmissionController;
 use App\Http\Controllers\Challenge\TypingChallengeController;
+use App\Http\Controllers\Challenge\TypingSubmissionController;
 use App\Http\Controllers\CtfCategoryController;
 use App\Http\Controllers\DifficultyController;
 use App\Http\Controllers\ProgrammingLanguageController;
@@ -17,7 +18,7 @@ Route::get('programming-languages/all', [ProgrammingLanguageController::class, '
 Route::middleware(['auth:sanctum', 'ability:admin:*'])->group(function () {
     Route::apiResource('challenges/ctf', CtfChallengeController::class)->except(['index', 'show']);
     Route::apiResource('challenges/coding', CodingChallengeController::class)->except(['index', 'show']);
-    Route::apiResource('challenges/typing', TypingChallengeController::class);
+    Route::apiResource('challenges/typing', TypingChallengeController::class)->except(['index', 'show']);
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -29,4 +30,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('challenges/coding/{coding}', [CodingChallengeController::class, 'show']);
     Route::post('challenges/coding/{coding}/execute-code', [ChallengeCodeExecutionController::class, 'executeCode']);
     Route::post('challenges/coding/{coding}/submit', [ChallengeCodeExecutionController::class, 'submitCode']);
+
+    Route::get('challenges/typing', [TypingChallengeController::class, 'index']);
+    Route::get('challenges/typing/{typing}', [TypingChallengeController::class, 'show']);
+    Route::post('challenges/typing/{typing}/submit', [TypingSubmissionController::class, 'store']);
+    Route::get('challenges/typing/{typing}/submissions', [TypingSubmissionController::class, 'getSubmissionHistory']);
 });
