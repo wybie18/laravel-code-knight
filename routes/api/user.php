b783bf963461e2f452f682f51cb92d9931c98b5e
Wybie <lyrncofyn@gmail.com>
2025-10-25 23:25:55 +0800

    feat(user): add routes for user achievements and rank retrieval

--- routes/api/user.php
+++ routes/api/user.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\ProfileController;
+use App\Http\Controllers\UserActivityController;
 use App\Http\Controllers\UserStatsController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [ProfileController::class, 'show']);
     Route::get('/stats', [UserStatsController::class, 'index']);
+    Route::get('/activities/heatmap', [UserActivityController::class, 'getHeatmap']);
+    Route::get('/my-achievements', [UserStatsController::class, 'getUserAchievements']);
+    Route::get('/my-rank', [UserStatsController::class, 'getUserRank']);
 });
